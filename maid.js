8683a5a1fdef2cec457498f8c7c852b00c854559
NeR0V <34779811+NeR0V@users.noreply.github.com>
2017-12-29 15:52:45 -0200

    Update maid.js

--- maid.js
+++ maid.js
@@ -2,46 +2,6 @@
 
 const TOKEN = "MzkzNTQ3MzU4Mzg3ODMwNzk0.DR3XPQ.GxSHTCJ2leKtj8Z3AL7Voj-6vQk";
 const PREFIX = ".";
-const config = require('./package.json');
-
-const size    = config.colors;
-const rainbow = new Array(size);
-
-for (var i=0; i<size; i++) {
-  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
-  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
-
-  rainbow[i] = '#'+ red + green + blue;
-}
-
-function sin_to_hex(i, phase) {
-  var sin = Math.sin(Math.PI / size * 2 * i + phase);
-  var int = Math.floor(sin * 127) + 128;
-  var hex = int.toString(16);
-
-  return hex.length === 1 ? '0'+hex : hex;
-}
-
-let place = 0;
-const servers = config.servers;
-
-function changeColor() {
-  for (let index = 0; index < servers.length; ++index) {
-    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
-		.catch(console.error);
-
-    if(config.logging){
-      console.log(`[ColorChanger] Você mudou a cor para ${rainbow[place]} no servidor: ${servers[index]}`);
-    }
-    if(place == (size - 1)){
-      place = 0;
-    }else{
-      place++;
-    }
-  }
-}
-
 
 var fortunes = [
     "Sim",
