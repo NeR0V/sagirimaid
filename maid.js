3bfb35265fb0a793db25e056ee9411b1251339a5
NeR0V <34779811+NeR0V@users.noreply.github.com>
2017-12-29 17:13:02 -0200

    Update maid.js

--- maid.js
+++ maid.js
@@ -3,6 +3,8 @@
 const TOKEN = "MzkzNTQ3MzU4Mzg3ODMwNzk0.DR3XPQ.GxSHTCJ2leKtj8Z3AL7Voj-6vQk";
 const PREFIX = ".";
 
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
 var fortunes = [
     "Sim",
     "Nao",
@@ -10,10 +12,86 @@ var fortunes = [
     "Vai se fuder"
 ];
 
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
 var bot = new Discord.Client();
 var fs = require("fs");
 
 var userData = JSON.parse(fs.readFileSync("Storage/userData.json", "utf8"));
+
+var data = require("./data.json");
+var options = require("./options.json");
+var auth = require("./auth.json");
+
+var afgdRole;
+var i = 0;
+
+var colours = [];
+var coloursIndex = 0;
+
+var interpolation = 1 / options.nameColourInterpolation;
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+setInterval(function() {
+    if (afgdRole) {
+        bot.updateRole(afgdRole, {
+            color: colours[coloursIndex]
+        });
+        coloursIndex++;
+        if (coloursIndex >= colours.length) coloursIndex = 0;
+    }
+}, options.nameColourSpeed)
+
+bot.on("message", function(msg) {
+
+    if (!afgdRole) {
+        console.log("for");
+        while (i < 1) {
+            i = i + interpolation;
+            console.log("for'd " + i + " " + interpolation);
+            colours.push(Number("0x" + RGBToHex(HSVtoRGB(i, 1, 1).r, HSVtoRGB(i, 1, 1).g, HSVtoRGB(i, 1, 1).b)));
+        } 
+        var roles = msg.channel.server.rolesOfUser(bot.user).filter(function(role) {
+            return role.name === "AFGD";
+        });
+        afgdRole = roles[0];
+    }
+});
+
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
+
+RGBToHex = function(r,g,b){
+    var bin = r << 16 | g << 8 | b;
+    return (function(h){
+        return new Array(7-h.length).join("0")+h
+    })(bin.toString(16).toUpperCase())
+}
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 bot.on("guildMemberAdd", function (member) {
 
@@ -30,6 +108,7 @@ if(msg === prefix + "add") {
     .setImage("https://78.media.tumblr.com/37f88d4b8d538adf80049d5ffefad2e1/tumblr_ouqgu127i01wwga3uo1_500.gif");
 });
 
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 bot.on("ready", function () {
     console.log("Carregando...");
@@ -39,6 +118,8 @@ bot.on("ready", function () {
     bot.user.setGame('Peçam ajuda para mim <3');
 
 });
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------
 
     bot.on("message", function (message) {
         if (message.author.equals(bot.user)) return;
@@ -70,6 +151,8 @@ bot.on("ready", function () {
             if (err) console.error(err);
         });
 
+        // -----------------------------------------------------------------------------------------------------------------------------------------------------
+
         if (message.content === "eae") {
             message.channel.sendMessage("suave?");
         }
@@ -87,6 +170,8 @@ bot.on("ready", function () {
         }
 
         if (!message.content.startsWith(PREFIX)) return;
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------------
 
         var args = message.content.substring(PREFIX.length).split(" ");
 
@@ -124,9 +209,7 @@ bot.on("ready", function () {
                     .setImage("https://media.giphy.com/media/IjmMzurYulKEw/giphy.gif");
                 message.channel.sendEmbed(embed);
                 break;
-
-
-
+            
         }
     });
 
